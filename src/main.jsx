b29c38f4f0aacca4b7744d3b69b1e94ca403ae7d
Sullivan Ford <sullivan.ford@bt.com>
2021-02-26 21:13:45 +0000

    Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './reset.css';
+
+ReactDOM.render(
+  <div>App working!</div>,
+  document.getElementById('root'),
+);
---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const sass = require('sass');
+
+const devMode = process.env.NODE_ENV !== 'production';
+
+module.exports = {
+  entry: './src/main.jsx',
+  output: {
+    path: path.join(__dirname, '/dist/'),
+    filename: 'bundle.js',
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.js[x]?$/,
+        exclude: /node_modules/,
+        resolve: {
+          extensions: ['.js', '.jsx'],
+        },
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react'],
+          },
+        },
+      },
+      {
+        test: /\.(c|s[ac])ss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              implementation: sass,
+            },
+          }],
+      },
+      {
+        test: /\.(eot|woff|woff2|svg|ttf)([?]?.*)$/,
+        use: ['file-loader'],
+      },
+    ],
+  },
+  plugins: [new HtmlWebPackPlugin({ template: 'src/index.html' })],
+  mode: devMode ? 'development' : 'production',
+};
