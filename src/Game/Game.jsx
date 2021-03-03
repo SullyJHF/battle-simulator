8da5d0c6d307e37895f08cea87733d0844336406
Sullivan Ford <sullivan.ford@bt.com>
2021-03-03 09:15:40 +0000

    Add game over screen

--- src/Game/Game.jsx
+++ src/Game/Game.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './actor.scss';
 import { Controls } from './Controls/Controls';
 import './game.scss';
+import { LOSE, useGame, WIN } from './gameSlice';
 import { Monster } from './Monster/Monster';
 import { Player } from './Player/Player';
 
@@ -13,5 +14,26 @@ export const Game = () => (
       <Controls />
       <Monster />
     </div>
+    <GameOver />
   </div>
 );
+
+const GameOver = () => {
+  const { phase } = useGame();
+  if (phase !== WIN && phase !== LOSE) return null;
+
+  const gameOverText = phase === WIN ? 'You Win!' : 'Game Over';
+
+  return (
+    <div id="game-over">
+      <h1 className={phase}>{gameOverText}</h1>
+      <button
+        className="button"
+        type="button"
+        onClick={() => console.log('reset')}
+      >
+        Play Again?
+      </button>
+    </div>
+  );
+};
